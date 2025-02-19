7e3e18af76757372bacb1299d4e7d9e37619fa3a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-18 21:54:14 -0300

    comitando os exercícios 1, 2, 3 e 4

---
+++ aulaRevisao.js
@@ -0,0 +1,22 @@
+// Exercício 1
+const dobro = (a) => a * 2;
+console.log(dobro(7)); // 14
+console.log(dobro(15)); // 30
+
+// Exercício 2
+const dividir = (a, b) => a / b;
+console.log(dividir(10, 2)); // 5
+console.log(dividir(9, 3));  // 3
+
+// Exercício 3
+const mensagemBoasVindas = (a) => ("Seja bem-vindo, " + a + "!");
+console.log(mensagemBoasVindas("Carlos")); // "Seja bem-vindo, Carlos!"
+console.log(mensagemBoasVindas("Ana"));    // "Seja bem-vindo, Ana!"
+
+// Exercício 4
+
+console.log(executarOperacao(6, 3, (a, b) => a - b)); // 3
+console.log(executarOperacao(4, 5, (a, b) => a * b)); // 20
+
+
+
